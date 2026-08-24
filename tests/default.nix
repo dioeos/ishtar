f92cb07de33ac586a137c1f14450d512c8f07322
Ryan Dioneda <er.dioneda@gmail.com>
2026-08-24 14:58:05 -0400

    Fix containers.nix test to inherit pkgs and inputs

--- tests/default.nix
+++ tests/default.nix
@@ -3,5 +3,5 @@
 {
   waits-for-tailscale = import ./waits-for-tailscale.nix { inherit pkgs; };
   fast-note-sync-service = import ./fast-note-sync-service.nix { inherit pkgs inputs; };
-  # containers = import ./containers.nix;
+  containers = import ./containers.nix { inherit pkgs inputs; };
 }
