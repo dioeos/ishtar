4a301a1a92bcc2ce83895f6225b1015ce91b55b1
Ryan Dioneda <er.dioneda@gmail.com>
2026-08-24 22:54:37 -0400

    Change mc server to be fabric with basic mods

--- modules/services/minecraft/default.nix
+++ modules/services/minecraft/default.nix
@@ -1,10 +1,20 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 
 {
   systemd.tmpfiles.rules = [
     "d /var/lib/podman-captain/minecraft-server 0750 podman-captain podman-captain -"
     "d /var/lib/podman-captain/minecraft-server/data 0750 podman-captain podman-captain -"
   ];
+  sops.secrets.minecraft-whitelist = {};
+  sops.templates."minecraft-env-whitelist" = {
+    owner = "podman-captain";
+    group = "podman-captain";
+    mode = "0400";
+
+    content = ''
+      WHITELIST=${config.sops.placeholder.minecraft-whitelist}
+    '';
+  };
   home-manager.users.podman-captain = {
     imports = [ inputs.quadlet-nix.homeManagerModules.quadlet ];
 
@@ -17,10 +27,34 @@
 
         environments = {
           EULA = "TRUE";
-          TYPE = "PAPER";
-          VERSION = "LATEST";
+          TYPE = "FABRIC";
+          VERSION = "26.2";
           MEMORY = "4G";
+
+          MOTD = "Ishtar";
+          GAMEMODE = "survival";
+          DIFFICULTY = "hard";
+
+          VIEW_DISTANCE = "10";
+          SIMULATION_DISTANCE = "8";
+
+          ONLINE_MODE = "TRUE";
+          ENABLE_WHITELIST = "TRUE";
+
+          PVP = "TRUE";
+
+          MODRINTH_PROJECTS = ''
+            fabric-api
+            lithium
+            mods-command
+            adventure-platform-mod
+            discord-mc-chat
+          '';
         };
+
+        environmentFiles = [
+          config.sops.templates."minecraft-env-whitelist".path
+        ];
 
         volumes = [
           "/var/lib/podman-captain/minecraft-server/data:/data"
